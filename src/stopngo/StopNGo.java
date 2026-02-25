61dc901bf4fcb638e9000dfbde0c016edc290823
Hxckfast <lg.dithuge@gmail.com>
2026-02-25 14:18:28 +0200

    Final Submission

---
+++ src/stopngo/StopNGo.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+/**
+ * An application showing stop and go signs using ascii characters
+*/
+
+package stopngo;
+
+/**
+ *
+ * @author ldith
+ */
+public class StopNGo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            System.out.println("\t\t\t -------------\n");
+            System.out.println("\t    ********    \t\t    XXXXXXXX   "); //Prints 1st line of signs
+            System.out.println("\t  *          *  \t\t  X          X ");
+            System.out.println("\t *  S T O P   * \t\t X     GO     X"); // Add text to the signs
+            System.out.println("\t  *          *  \t\t  X          X ");
+            System.out.println("\t    ********    \t\t    XXXXXXXX   ");
+            System.out.println("\t       **       \t\t       XX      ");//Printing the stem
+            System.out.println("\t       **       \t\t       XX      ");
+            System.out.println("\t       **       \t\t       XX      ");
+            System.out.println("\t       **       \t\t       XX      ");
+            System.out.println("\t      ****      \t\t      XXXX     ");
+            System.out.println("\t   **********   \t\t   XXXXXXXXXX  ");// Printing out the base
+            System.out.println("|------------------------------------------------------|");
+            System.out.println("|------------------------------------------------------|");//Done
+            
+    }
+    
+}
